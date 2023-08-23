94270b62b8f3fff26ac821ce124ebbadb654dfb7
andreodon2 <andreodon@gmail.com>
2023-08-23 15:08:02 -0300

    inclusão das aulas 2 e 3

---
+++ aula 2 - variáveis/script.js
@@ -0,0 +1,25 @@
+// tipos primitivos
+
+// boolean
+var vOuF = false;
+
+console.log(vOuF);
+console.log(typeof(vOuF));
+
+// number
+
+var numeroQualquer = 1;
+
+console.log(1);
+console.log(typeof(1));
+
+// string
+var nome = 'andre';
+
+console.log(nome);
+console.log(typeof(nome));
+
+// como declarar
+
+var variavel;
+console.log(variavel);
---
+++ aula 3 - array e object/script.js
@@ -0,0 +1,9 @@
+// O QUE SÃO VETORES OU ARRAYS
+
+// como declarar
+
+let array = ['string', 1, true];
+console.log(array);
+
+let array2 = ['string', true, 1, ['aray1', 'array2', 'array3'], ['array4']];
+console.log(array2);
