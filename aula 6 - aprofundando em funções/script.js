1ea26531dd5467480f17609468aebe2b9ae1f532
andreodon2 <andreodon@gmail.com>
2023-08-25 16:38:40 -0300

    Inclusão e conclusão da aula 6

---
+++ aula 6 - aprofundando em funções/script.js
@@ -0,0 +1,23 @@
+// DECLARAÇÃO DA FUNÇÃO
+
+/*function funcao() {
+    console.log('Tudo certo jovem!');
+}
+
+funcao();*/
+
+// EXPRESSÕES DE FUNÇÕES - São funções que são atribuídas a expressões. A nomeação da função é opcional
+
+/*var funcao = function funcao() {
+    console.log('Sou uma mensagem de função de expressão')
+}
+
+funcao();*/
+
+// ARROW FUNCTION - São de sintaxe curta. Anônimas, sendo declaradas entre () seguido de '=>' e depois as chaves.
+
+var funcao = () => {
+    console.log('Sou uma arrow function');
+}
+
+funcao();
